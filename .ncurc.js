145ec3818bf2fd216429a82431a9be9c89386bc6
Andris Reinman <andris@reinman.eu>
2024-02-18 14:20:10 +0200

    fix(deps): updated deps

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,10 @@ module.exports = {
     upgrade: true,
     reject: [
         // no support for Node 16
-        'marked'
+        'marked',
+
+        // esm issues
+        'js-beautify',
+        'mjml'
     ]
 };
